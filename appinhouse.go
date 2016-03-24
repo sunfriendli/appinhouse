c09da0d9d7a94faf857724ffe9f207047d045c3b
jianghaitao1221 <jianghaitao@kingsoft.com>
2016-03-24 12:58:46 +0800

    注释掉静态资源

--- appinhouse.go
+++ appinhouse.go
@@ -17,9 +17,9 @@ func main() {
 	beego.SetLevel(beego.LevelInformational)
 	beego.SetLogFuncCall(true)
 	beego.SetLogger("file", `{"filename":"appinhouse.log"}`)
-	beego.SetStaticPath(Android_Release_Static_Path, controllers.GetDownDirectory(Android, Release))
-	beego.SetStaticPath(Android_Dev_Static_Path, controllers.GetDownDirectory(Android, Dev))
-	beego.SetStaticPath(Ios_Release_Static_Path, controllers.GetDownDirectory(Ios, Release))
-	beego.SetStaticPath(Ios_Dev_Static_Path, controllers.GetDownDirectory(Ios, Dev))
+	//	beego.SetStaticPath(Android_Release_Static_Path, controllers.GetDownDirectory(Android, Release))
+	//	beego.SetStaticPath(Android_Dev_Static_Path, controllers.GetDownDirectory(Android, Dev))
+	//	beego.SetStaticPath(Ios_Release_Static_Path, controllers.GetDownDirectory(Ios, Release))
+	//	beego.SetStaticPath(Ios_Dev_Static_Path, controllers.GetDownDirectory(Ios, Dev))
 	beego.Run()
 }
--- routers/router.go
+++ routers/router.go
@@ -8,12 +8,12 @@ import (
 
 func init() {
 	beego.Router("/", &controllers.MainController{}, "get:Index")
-	beego.Router("/v1/mobile/last", &controllers.MainController{}, "get:Last")
-	beego.Router("/v1/mobile/list/dev", &controllers.MainController{}, "get:DevList")
-	beego.Router("/v1/mobile/list/release", &controllers.MainController{}, "get:ReleaseList")
-	beego.Router("/v1/last", &controllers.MainController{}, "get:Last")
-	beego.Router("/v1/list/android/dev", &controllers.MainController{}, "get:AndroidDevList")
-	beego.Router("/v1/list/android/release", &controllers.MainController{}, "get:AndroidReleaseList")
-	beego.Router("/v1/list/ios/dev", &controllers.MainController{}, "get:IosDevList")
-	beego.Router("/v1/list/ios/release", &controllers.MainController{}, "get:IosReleaseList")
+	beego.Router("/api/mobile/last", &controllers.MainController{}, "get:Last")
+	beego.Router("/api/mobile/list/dev", &controllers.MainController{}, "get:DevList")
+	beego.Router("/api/mobile/list/release", &controllers.MainController{}, "get:ReleaseList")
+	beego.Router("/api/last", &controllers.MainController{}, "get:Last")
+	beego.Router("/api/list/android/dev", &controllers.MainController{}, "get:AndroidDevList")
+	beego.Router("/api/list/android/release", &controllers.MainController{}, "get:AndroidReleaseList")
+	beego.Router("/api/list/ios/dev", &controllers.MainController{}, "get:IosDevList")
+	beego.Router("/api/list/ios/release", &controllers.MainController{}, "get:IosReleaseList")
 }
