abd6c2402aa9fbc999f0c9575777163430280d90
lemon-li <melefans@gmail.com>
2018-12-29 16:51:15 +0800

    delete obsolete Web static assets

--- web/README.md
+++
@@ -1,6 +0,0 @@
-## web
-目录`static`提供前端的静态资源。web采用bootstrap模板。
-
-#### 模板地址
-https://github.com/silverbux/bootflat-admin
-
--- web/static/js/timezone.js
+++
@@ -1,5 +0,0 @@
- function getTimeOffset(){
-    var d = new Date()
-    var offset=d.getTimezoneOffset()
-    return -1*offset
- }
