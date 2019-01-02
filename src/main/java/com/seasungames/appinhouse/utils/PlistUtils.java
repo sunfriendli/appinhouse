3864ae1be61ac681761d6103a46274b9a4790797
lemon-li <melefans@gmail.com>
2019-01-02 17:35:54 +0800

    plist annotation

--- src/main/java/com/seasungames/appinhouse/utils/PlistUtils.java
+++ src/main/java/com/seasungames/appinhouse/utils/PlistUtils.java
@@ -2,6 +2,41 @@ package com.seasungames.appinhouse.utils;
 
 import com.dd.plist.*;
 
+/***
+ <?xml version="1.0" encoding="UTF-8"?>
+ <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+ <plist version="1.0">
+    <dict>
+        <key>items</key>
+        <array>
+            <dict>
+                <key>assets</key>
+                <array>
+                    <dict>
+                        <key>kind</key>
+                        <string>software-package</string>
+                        <key>url</key>
+                        <string>https://jenkins.intranet.rog2.org/job/Personal/job/liujun8/job/pirates-ios/lastSuccessfulBuild/artifact/pirates-personal-trunk-b9-20181225-development.ipa</string>
+                    </dict>
+                </array>
+                <key>metadata</key>
+                <dict>
+                    <key>bundle-identifier</key>
+                    <string>cn.com.xishanju.pirates</string>
+                    <key>bundle-version</key>
+                    <string>1.0</string>
+                    <key>kind</key>
+                    <string>software</string>
+                    <key>title</key>
+                    <string>加勒比海盗</string>
+                </dict>
+            </dict>
+        </array>
+    </dict>
+ </plist>
+
+ * */
+
 public class PlistUtils {
 
     public static String GenPlist(String url, String bundle_id, String title) {
