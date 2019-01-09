67bd7611688bf9bd87c80b8dd8493e123729ed43
lee <melefans@gmail.com>
2019-01-09 22:52:40 +0800

    add responseVo as http response

--- src/main/java/com/seasungames/appinhouse/routes/handlers/RouteFailureHandler.java
+++ src/main/java/com/seasungames/appinhouse/routes/handlers/RouteFailureHandler.java
@@ -1,9 +1,9 @@
 package com.seasungames.appinhouse.routes.handlers;
 
 import com.amazonaws.services.dynamodbv2.model.AmazonDynamoDBException;
+import com.seasungames.appinhouse.models.ResponseVo;
 import com.seasungames.appinhouse.routes.exception.HttpException;
 import io.vertx.core.Handler;
-import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.RoutingContext;
@@ -23,21 +23,19 @@ public class RouteFailureHandler implements Handler<RoutingContext> {
         Throwable failure = rc.failure();
 
         if (failure instanceof ValidationException) {
-            toResponseJson(rc, 400, errorMessageToErrorBody(rc, "Validation failed"));
+            toResponseJson(rc, 400, errorMessageToErrorBody("Validation failed"));
         } else if (failure instanceof AmazonDynamoDBException) {
-            toResponseJson(rc, 400, errorMessageToErrorBody(rc, errorMessageToErrorBody(rc, failure.getMessage())));
+            toResponseJson(rc, 400, errorMessageToErrorBody(failure.getMessage()));
         } else if (failure instanceof HttpException) {
-            toResponseJson(rc, ((HttpException) failure).status, errorMessageToErrorBody(rc, failure.getMessage()));
-        }  else {
-            toResponseJson(rc, 500, errorMessageToErrorBody(rc, failure.getMessage()));
+            toResponseJson(rc, ((HttpException) failure).status, errorMessageToErrorBody(failure.getMessage()));
+        } else {
+            toResponseJson(rc, 500, errorMessageToErrorBody(failure.getMessage()));
         }
 
         LOG.error("RouteError - {}: {}", failure.getClass().getSimpleName(), failure.getMessage());
     }
 
-    private String errorMessageToErrorBody(RoutingContext rc, String message) {
-        return new JsonObject()
-                .put("path", rc.normalisedPath())
-                .put("message", message).toString();
+    private String errorMessageToErrorBody(String message) {
+        return new ResponseVo().setMessage(message).toJson();
     }
 }
