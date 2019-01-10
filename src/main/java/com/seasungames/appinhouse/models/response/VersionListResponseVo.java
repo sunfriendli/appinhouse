59b196bc43b05960b172a6b791af89d1935525f4
lemon-li <melefans@gmail.com>
2019-01-10 12:40:15 +0800

    Redesign , refer to MVC pattern

---
+++ src/main/java/com/seasungames/appinhouse/models/response/VersionListResponseVo.java
@@ -0,0 +1,49 @@
+package com.seasungames.appinhouse.models.response;
+
+import java.util.Map;
+
+/**
+ * Created by lile on 1/10/2019
+ */
+public class VersionListResponseVo {
+
+    private String Id;
+
+    private String platform;
+
+    private String version;
+
+    private Map<String,Object> info;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Map<String, Object> getInfo() {
+        return info;
+    }
+
+    public void setInfo(Map<String, Object> info) {
+        this.info = info;
+    }
+}
