7d00ebbe3f00df48f7d53044bc496b1e6df2e944
lemon-li <melefans@gmail.com>
2019-01-15 13:35:14 +0800

    remove obsolete dynamodb service code

--- src/main/java/com/seasungames/appinhouse/stores/services/package-info.java
+++
@@ -1,4 +0,0 @@
-@ModuleGen(groupPackage = "com.seasungames.appinhouse.stores.services", name = "dynamodb-service")
-package com.seasungames.appinhouse.stores.services;
-
-import io.vertx.codegen.annotations.ModuleGen;
