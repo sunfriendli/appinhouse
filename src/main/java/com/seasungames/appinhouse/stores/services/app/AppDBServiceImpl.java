39c8b6dde6b206016ed6a3fa1ae9062924ef5f26
lemon-li <melefans@gmail.com>
2019-01-16 16:54:28 +0800

    Add: db exception handler

--- src/main/java/com/seasungames/appinhouse/stores/services/app/AppDBServiceImpl.java
+++ src/main/java/com/seasungames/appinhouse/stores/services/app/AppDBServiceImpl.java
@@ -1,10 +1,13 @@
 package com.seasungames.appinhouse.stores.services.app;
 
 import com.seasungames.appinhouse.dagger.scope.AppInHouse;
+import com.seasungames.appinhouse.routes.exception.DBException;
+import com.seasungames.appinhouse.stores.AppStore;
 import com.seasungames.appinhouse.stores.services.app.models.AppListResponseVo;
 import com.seasungames.appinhouse.stores.services.app.models.AppResponseVo;
 import com.seasungames.appinhouse.stores.services.app.models.AppVo;
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 
 import javax.inject.Inject;
@@ -16,33 +19,61 @@ import javax.inject.Inject;
 public class AppDBServiceImpl implements AppDBService {
 
     @Inject
+    AppStore appStore;
+
+    @Inject
     public AppDBServiceImpl() {
 
     }
 
     @Override
     public void getAppsList(String lastKey, Handler<AsyncResult<AppListResponseVo>> resultHandler) {
-
+        try {
+            AppListResponseVo result = appStore.getAppsList(lastKey);
+            resultHandler.handle(Future.succeededFuture(result));
+        } catch (Exception e) {
+            resultHandler.handle(DBException.fail(e));
+        }
     }
 
     @Override
     public void updateApps(AppVo vo, Handler<AsyncResult<AppResponseVo>> resultHandler) {
-
+        try {
+            AppResponseVo result = appStore.updateApps(vo);
+            resultHandler.handle(Future.succeededFuture(result));
+        } catch (Exception e) {
+            resultHandler.handle(DBException.fail(e));
+        }
     }
 
     @Override
     public void createApps(AppVo vo, Handler<AsyncResult<Void>> resultHandler) {
-
+        try {
+            appStore.createApps(vo);
+            resultHandler.handle(Future.succeededFuture());
+        } catch (Exception e) {
+            resultHandler.handle(DBException.fail(e));
+        }
     }
 
     @Override
     public void deleteApps(String appId, Handler<AsyncResult<Void>> resultHandler) {
-
+        try {
+            appStore.deleteApps(appId);
+            resultHandler.handle(Future.succeededFuture());
+        } catch (Exception e) {
+            resultHandler.handle(DBException.fail(e));
+        }
     }
 
     @Override
     public void getApps(String appId, Handler<AsyncResult<AppResponseVo>> resultHandler) {
-
+        try {
+            AppResponseVo result = appStore.getApps(appId);
+            resultHandler.handle(Future.succeededFuture(result));
+        } catch (Exception e) {
+            resultHandler.handle(DBException.fail(e));
+        }
     }
 
     @Override
--- src/main/java/com/seasungames/appinhouse/stores/services/version/VersionDBServiceImpl.java
+++ src/main/java/com/seasungames/appinhouse/stores/services/version/VersionDBServiceImpl.java
@@ -1,9 +1,12 @@
 package com.seasungames.appinhouse.stores.services.version;
 
 import com.seasungames.appinhouse.dagger.scope.AppInHouse;
+import com.seasungames.appinhouse.routes.exception.DBException;
+import com.seasungames.appinhouse.stores.VersionStore;
 import com.seasungames.appinhouse.stores.services.version.models.VersionVo;
 import com.seasungames.appinhouse.stores.services.version.models.VersionResponseVo;
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 
 import javax.inject.Inject;
@@ -16,28 +19,51 @@ import java.util.List;
 public class VersionDBServiceImpl implements VersionDBService {
 
     @Inject
+    VersionStore versionStore;
+
+    @Inject
     public VersionDBServiceImpl() {
 
     }
 
     @Override
     public void getPlatformList(String appId, String platform, Handler<AsyncResult<List<VersionResponseVo>>> resultHandler) {
-
+        try {
+            List<VersionResponseVo> versionResponseVos = versionStore.getPlatformList(appId, platform);
+            resultHandler.handle(Future.succeededFuture(versionResponseVos));
+        } catch (Exception e) {
+            resultHandler.handle(DBException.fail(e));
+        }
     }
 
     @Override
     public void getLatestList(String appId, Handler<AsyncResult<List<VersionResponseVo>>> resultHandler) {
-
+        try {
+            List<VersionResponseVo> versionResponseVos = versionStore.getLatestList(appId);
+            resultHandler.handle(Future.succeededFuture(versionResponseVos));
+        } catch (Exception e) {
+            resultHandler.handle(DBException.fail(e));
+        }
     }
 
     @Override
     public void createVersion(VersionVo vo, Handler<AsyncResult<Void>> resultHandler) {
-
+        try {
+            versionStore.createVersion(vo);
+            resultHandler.handle(Future.succeededFuture());
+        } catch (Exception e) {
+            resultHandler.handle(DBException.fail(e));
+        }
     }
 
     @Override
     public void getVersion(String appId, String platform, String version, Handler<AsyncResult<VersionVo>> resultHandler) {
-
+        try {
+            VersionVo vo = versionStore.getVersion(appId, platform, version);
+            resultHandler.handle(Future.succeededFuture(vo));
+        } catch (Exception e) {
+            resultHandler.handle(DBException.fail(e));
+        }
     }
 
     @Override
