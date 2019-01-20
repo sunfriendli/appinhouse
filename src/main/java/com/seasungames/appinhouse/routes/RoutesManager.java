9ad11ae661866d624ae626e6005eed3552224aa6
lee <melefans@gmail.com>
2019-01-20 19:06:50 +0800

    add CORS configure

--- src/main/java/com/seasungames/appinhouse/routes/RoutesManager.java
+++ src/main/java/com/seasungames/appinhouse/routes/RoutesManager.java
@@ -7,6 +7,7 @@ import com.seasungames.appinhouse.dagger.common.scope.AppInHouse;
 import com.seasungames.appinhouse.routes.exception.impl.BadRequestException;
 import com.seasungames.appinhouse.routes.handlers.impl.RouteFailureHandler;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
@@ -15,11 +16,14 @@ import io.vertx.ext.web.Router;
 import com.seasungames.appinhouse.routes.handlers.impl.RouteAppHandler;
 import com.seasungames.appinhouse.routes.handlers.impl.RouteVersionHandler;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.CorsHandler;
 import io.vertx.ext.web.handler.LoggerHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.HashSet;
+import java.util.Set;
 
 /***
  RESTful API
@@ -63,8 +67,33 @@ public class RoutesManager implements Async {
             .setWebRoot(APIConstant.WEBROOT)
             .setIndexPage("index.html")
             .setDefaultContentEncoding("UTF-8"));
+        router.route().handler(CorsHandler.create("*")
+            .allowedHeaders(getCORSHeaders())
+            .allowedMethods(getCORSMethods()));
         router.route().failureHandler(new RouteFailureHandler());
         router.route().last().handler(rc -> rc.fail(new BadRequestException()));
+    }
+
+    private Set<String> getCORSHeaders() {
+        Set<String> allowedHeaders = new HashSet<>();
+        allowedHeaders.add("x-requested-with");
+        allowedHeaders.add("Access-Control-Allow-Origin");
+        allowedHeaders.add("origin");
+        allowedHeaders.add("Content-Type");
+        allowedHeaders.add("accept");
+        allowedHeaders.add("X-PINGARUNER");
+        return allowedHeaders;
+    }
+
+    private Set<HttpMethod> getCORSMethods() {
+        Set<HttpMethod> allowedMethods = new HashSet<>();
+        allowedMethods.add(HttpMethod.GET);
+        allowedMethods.add(HttpMethod.POST);
+        allowedMethods.add(HttpMethod.OPTIONS);
+        allowedMethods.add(HttpMethod.DELETE);
+        allowedMethods.add(HttpMethod.PATCH);
+        allowedMethods.add(HttpMethod.PUT);
+        return allowedMethods;
     }
 
     @Override
